8cad89f5762df9250616e12799ea719846705442
ayushmanitiwari931@gmail.com <ayushmanitiwari931@gmail.com>
2025-08-28 14:48:31 +0530

    day 3

--- day_3.py
+++ day_3.py
@@ -16,3 +16,54 @@ print(id(var3))
 print(id(var4))
 
 
+# String data type
+
+#multiline string
+a="""jgasjgasvdjhavd 
+dbjasvdhjdhjad
+anmdvjhadvhkDB"""
+
+K='''JAHVDJGASVDJHASD
+ASKJDHVBJHASVDHJA'''
+
+
+#Single line string
+b="vahscSDAHGDAH"
+
+
+#STRING LENGTH
+print(len(a))
+
+print(a[4])
+
+
+#CHECK STRING
+txt="hi i am just building and python program"
+print("i" in txt)
+print("i" not in txt)
+
+print("rajat" in txt)
+
+
+# SLICING STRING
+
+b= "hello world"
+print(b[2:])
+print(b[:3])
+print(b[0:5])
+print(b[-4:-2])
+
+
+
+#MODIFY STRING
+
+a="heLLo"
+print(a.upper())
+print(a.lower())
+
+
+#REMOVE WHITESPACES
+
+a="   hello world    "
+print(a)
+print(a.strip())
