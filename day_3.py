654713a425b53c10f4e63f3d2e30db7e1490fcac
ayushmanitiwari931@gmail.com <ayushmanitiwari931@gmail.com>
2025-08-28 14:24:36 +0530

    data type

---
+++ day_3.py
@@ -0,0 +1,18 @@
+# numeric data type
+
+var1=1
+var2=True
+var3=10.023
+var4=10+3j
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+print(id(var1))
+print(id(var2))
+print(id(var3))
+print(id(var4))
+
+
