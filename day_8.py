fb9c71aaf0dc37c6f3aef0165af9332b3baf8f99
ayushmanitiwari931@gmail.com <ayushmanitiwari931@gmail.com>
2025-09-17 09:38:26 +0530

    list

--- day_8.py
+++ day_8.py
@@ -50,3 +50,46 @@ list4.pop()  #removes last element
 list4.pop(1) #removes element at index 1
 print(list4)
 
+
+#del function
+# del list4 
+# print(list4)
+
+#clear function
+list5=[1,2,3,4,5]
+list5.clear()
+print(list5)
+
+#while loop
+list6=["a","b","c","d"]
+i=0
+while i<len(list6):
+    print(list6[i])
+    i+=1
+
+#for loop
+for item in list6:
+    print(item)
+
+fruits=["apple","banana","cherry"]
+newlist=[x.upper() for x in fruits]
+print(newlist)
+
+newlist2=[x.capitalize() for x in fruits]
+print(newlist2)
+
+if("apple" in fruits):
+    print("yes apple is present")
+
+#sort function
+fruits.sort()  #assending order
+print(fruits)
+
+fruits.sort(reverse=True) #descending order
+print(fruits)
+
+#reverse
+list0=["Apple","cherry","banana"]
+list0.reverse()
+print(list0)
+
